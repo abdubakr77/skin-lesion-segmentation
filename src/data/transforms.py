e08315bf5366a54281cf72167436cfa36b872a0b
abdubakr77 <abdubakora1232@gmail.com>
2026-09-02 23:26:30 +0300

    feat(data): add configurable augmentation pipeline
    - Add reusable transform builder for image augmentation
    - Support flips, rotation, brightness, contrast, hue, saturation, and CLAHE
    - Add shift and scale transformations with configurable probabilities
    - Preserve segmentation keypoints during geometric transformations# Please enter the commit message for your changes. Lines starting

--- src/data/transforms.py
+++ src/data/transforms.py
@@ -3,6 +3,7 @@
 import cv2
 import os
 import numpy as np
+import albumentations as A
 
 def read_image_and_label(filename_no_ext, data_yaml):
     img_path = os.path.join(data_yaml['train'], filename_no_ext + ".jpg")
@@ -26,3 +27,30 @@ def read_image_and_label(filename_no_ext, data_yaml):
             class_labels.append(cls_id)
 
     return image, polygons, class_labels
+
+
+def build_transform(config):
+    return A.Compose([
+        A.HorizontalFlip(p=config['hflip_p']),
+        A.VerticalFlip(p=config['vflip_p']),
+        A.Rotate(limit=config['rotate_limit'], p=config['rotate_p'], border_mode=cv2.BORDER_REFLECT_101),
+        A.RandomBrightnessContrast(
+            brightness_limit=config['brightness_limit'],
+            contrast_limit=config['contrast_limit'],
+            p=config['brightness_contrast_p']
+        ),
+        A.HueSaturationValue(
+            hue_shift_limit=config['hue_shift_limit'],
+            sat_shift_limit=config['sat_shift_limit'],
+            val_shift_limit=config['val_shift_limit'],
+            p=config['hue_sat_val_p']
+        ),
+        A.CLAHE(clip_limit=config['clahe_clip_limit'], p=config['clahe_p']),
+        A.ShiftScaleRotate(
+            shift_limit=config['shift_limit'],
+            scale_limit=config['scale_limit'],
+            rotate_limit=0,
+            p=config['shift_scale_rotate_p'],
+            border_mode=cv2.BORDER_REFLECT_101
+        ),
+    ], keypoint_params=A.KeypointParams(format='xy', remove_invisible=False))
