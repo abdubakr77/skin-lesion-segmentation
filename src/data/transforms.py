ab2a541beec31b159ad33afc33e48b793fdb6bc6
abdubakr77 <abdubakora1232@gmail.com>
2026-09-02 23:25:12 +0300

    feat(data): add YOLO image and label reader
    - Add utility to load YOLO images and segmentation labels
    - Convert loaded images from BGR to RGB format
    - Parse polygon coordinates and class IDs from label files
    - Return image data with polygon annotations and class labels

--- src/data/transforms.py
+++ src/data/transforms.py
@@ -1,1 +1,28 @@
-# Augmentation and preprocessing pipelines (train/val transforms).
+# Augmentation and preprocessing pipelines (train/val/test transforms).
+
+import cv2
+import os
+import numpy as np
+
+def read_image_and_label(filename_no_ext, data_yaml):
+    img_path = os.path.join(data_yaml['train'], filename_no_ext + ".jpg")
+    image = cv2.imread(img_path)
+    image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
+
+    labels_path = data_yaml['train'].replace('images', 'labels')
+    label_path = os.path.join(labels_path, filename_no_ext + ".txt")
+
+    polygons = []
+    class_labels = []
+
+    with open(label_path, 'r') as f:
+        for line in f.readlines():
+            values = line.split()
+            cls_id = int(float(values[0]))
+            coords = list(map(float, values[1:]))
+            points = np.array(coords).reshape(-1, 2)
+
+            polygons.append(points)
+            class_labels.append(cls_id)
+
+    return image, polygons, class_labels
